96f6139e66d2e71d87159c00012a9b076ec0b4b3
Ahmed Khamis <ahmedkhamees37@gmail.com>
2025-03-06 11:13:09 +0200

    examples

---
+++ Bash-Examples/backup.sh
@@ -0,0 +1,11 @@
+!/bin/bash
+
+# Author : Ahmed Khamis
+# Date Created : 6/3/2025
+# date Modified : 6/3/2025
+
+# Description : Create a backup from /etc/   in my location
+
+
+tar -cvf backup.tar /etc/
+exit 0
---
+++ Bash-Examples/print first script.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Author : Ahmed Khamis
+# Date Created : 6/3/2025
+# date Modified : 6/3/2025
+
+# Description : print first script
+
+
+
+echo "First script ........."
